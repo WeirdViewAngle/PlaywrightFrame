a8ef2dabbb4f16bf946b25ded4bcb19a0fc253a3
WindowsKek <WindowsKek@DESKTOP-O5FVDUO>
2023-06-29 21:53:50 +0400

    Second

---
+++ PlaywriteFramework/Elements/Pages/GogolPage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywriteFramework.Elements.Pages
+{
+    public class GogolPage
+    {
+        private readonly IPage page;
+
+        public GogolPage(IPage page)
+        {
+            this.page = page;
+        }
+
+
+    }
+}
